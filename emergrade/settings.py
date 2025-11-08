a636eea93fa3c4099fb3553a39df2b2e10827a6a
Ian Lartey <ianlartey@Ians-MacBook-Air-2.local>
2025-11-08 01:34:58 -0700

    Landing page 1.0 done

--- emergrade/settings.py
+++ emergrade/settings.py
@@ -34,8 +34,12 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'vton',
+    'impulse_monitoring',
 ]
 
 MIDDLEWARE = [
@@ -53,7 +57,7 @@ ROOT_URLCONF = 'emergrade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ impulse_monitoring/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ImpulseMonitoringConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'impulse_monitoring'
